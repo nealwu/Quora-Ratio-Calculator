63e3ed63aaad5c39610eacc0bd548db6aceb6c29
Neal Wu <neal@nealwu.com>
2013-09-27 01:28:28 -0400

    Calculation fix and start working on board posts

--- ratio.js
+++ ratio.js
@@ -13,11 +13,11 @@ if (checkThousand(upvotesString)) {
     upvotesString = $('.more_link')[0].innerHTML;
     upvotes = parseInt(upvotesString) + 3;
 } else {
-    upvotes = parseInt(upvotesString);
+    upvotes = parseInt(upvotesString) - 1;
 }
 
 var views = parseInt(removeCommas($('span:contains(" views")').children()[0].innerHTML));
-var ratio = 100.0 * (upvotes - 1) / views;
+var ratio = 100.0 * upvotes / views;
 var ratioString = '<strong>' + ratio.toFixed(2) + '%</strong> upvote-to-view ratio';
 $('.view_count').append($('<div>').html(ratioString));
-$('.answer_voters').append($('<div>').html(ratioString));
+$('.answer_voters, .board_item_voters').append($('<div>').html(ratioString));
