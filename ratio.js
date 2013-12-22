60aba82fcdcb486d8c385de5561c7e0dc7a76da4
Neal Wu <neal@nealwu.com>
2013-12-21 23:56:58 -0600

    Change upvotes to the number displayed

--- ratio.js
+++ ratio.js
@@ -10,13 +10,16 @@ var upvotes = 0;
 var upvotesString = $('.numbers')[0].innerHTML;
 
 if (checkThousand(upvotesString)) {
-    upvotesString = $('.more_link')[0].innerHTML;
-    upvotes = parseInt(upvotesString) + 3;
+    upvotesString = $('.more_link:contains(" more")')[0].innerHTML;
+    upvotes = parseInt(upvotesString) + 4;
 } else {
-    upvotes = parseInt(upvotesString) - 1;
+    upvotes = parseInt(upvotesString);
 }
 
-var views = parseInt(removeCommas($('span:contains(" views")').children()[0].innerHTML));
+var viewsString = $('span:contains(" views")');
+viewsString = viewsString[viewsString.length - 1].innerHTML;
+var startIndex = viewsString.indexOf('<strong>') + 8;
+var views = parseInt(removeCommas(viewsString.substring(startIndex)));
 var ratio = 100.0 * upvotes / views;
 var ratioString = '<strong>' + ratio.toFixed(2) + '%</strong> upvote-to-view ratio';
 $('.view_count').append($('<div>').html(ratioString));
